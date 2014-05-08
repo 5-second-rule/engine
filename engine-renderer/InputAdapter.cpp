53740471a8a75542b1fd0a45767ba36e9fc56147
Amy <chandler.amym@gmail.com>
2014-05-08 00:47:15 -0700

    Now with gamepad control.

--- engine-renderer/InputAdapter.cpp
+++ engine-renderer/InputAdapter.cpp
@@ -2,10 +2,22 @@
 
 InputAdapter::InputAdapter() { }
 
+void InputAdapter::setInput(const Transmission::Input *input) {
+	this->input = input;
+}
+
 Transmission::Input::KeyState InputAdapter::getKeyState(Transmission::Input::Key key) {
 	return this->input->getKeyState(key);
 }
 
-void InputAdapter::setInput(Transmission::Input *input) {
-	this->input = input;
+float InputAdapter::getAnalogTriggerValue(Transmission::Input::Key key) {
+	return this->input->getAnalogTriggerValue(key);
+}
+
+std::pair<Common::Vector4, float> InputAdapter::getAnalogStickPosition(Transmission::Input::Key key) {
+	return this->input->getAnalogStickPosition(key);
+}
+
+bool InputAdapter::isControllerConnected() {
+	return this->input->isControllerConnected();
 }
