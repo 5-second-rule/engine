b580586d1e040130e64b4a28581b52371bc4abcb
Jake Maskiewicz <jakemaskiewicz@gmail.com>
2014-05-26 22:09:14 -0700

    reworked camera and added IMoveable references

--- engine-renderer/PlayerCameraHandler.cpp
+++ engine-renderer/PlayerCameraHandler.cpp
@@ -5,6 +5,7 @@ PlayerCameraHandler::PlayerCameraHandler()
 {
 	this->position = Common::Point(0,0,-10);
 	this->lookAt = Common::Point(0,0,0);
+	this->up = Common::Vector(0, 1, 0);
 }
 
 PlayerCameraHandler::~PlayerCameraHandler()
--- engine-renderer/RenderableObject.cpp
+++ engine-renderer/RenderableObject.cpp
@@ -12,10 +12,6 @@ void RenderableObject::render() {
 	this->model->draw();
 };
 
-void RenderableObject::setPosition(const float pos[3]) {
-	this->model->setPosition(Common::Point(pos[0],pos[1],pos[2]));
-}
-
-void RenderableObject::setRotation(const Vector4& rot) {
-	this->model->setRotation(rot.x(),rot.y(),rot.z());
+IMoveable* RenderableObject::getMoveable() {
+	return this->model;
 }
